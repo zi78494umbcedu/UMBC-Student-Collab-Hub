ee754e207960c691950340ab74dcccfcdf70bf82
Ashwin Gupta <zi78494@umbc.edu>
2023-12-18 04:33:28 -0500

    final2.6

--- server.js
+++ server.js
@@ -1,28 +1,17 @@
 const express = require('express');
-const session = require('express-session');
-const path = require('path');
 const bodyParser = require('body-parser');
-const { Pool } = require('pg');
+const mysql = require('mysql2');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Use Pool instead of createConnection
-const pool = new Pool({
-    user: 'ashwin',
-    host: 'scaled-canine-6605.g8z.cockroachlabs.cloud',
-    database: 'defaultdb',
-    password: 'y4VGudCsMKTPsjBlK52nFA',
-    port: 26257
-});
-
-//Connect to CockroachDB (PostgreSQL)
-pool.connect((err) => {
-    if (err) {
-        console.log('Error connecting to CockroachDB:', err);
-        return;
-    }
-    console.log('Connected to CockroachDB');
+// MySQL Connection Pool
+const pool = mysql.createPool({
+    connectionLimit: 10, // Adjust this based on your requirements
+    host: 'aws.connect.psdb.cloud',
+    user: 'hhyp6dujl3p38u81ixla',
+    password: 'pscale_pw_Sloclnj83hWTllvnzvAumBvFS5XCutuoWlNPLOKP4pP',
+    database: 'student_testimonials'
 });
 
 // Middleware to parse JSON and form data
@@ -35,20 +24,34 @@ app.post('/submitTestimonial', (req, res) => {
     const sql = 'INSERT INTO testimonials (name, email, role, category, story) VALUES (?, ?, ?, ?, ?)';
     const values = [name, email, role, category, story];
 
-    // Insert the testimonial into the database
-    pool.query(sql, values, (err, result) => {
+    // Acquire a connection from the pool
+    pool.getConnection((err, connection) => {
         if (err) {
-            console.error('Error inserting into CockroachDB:', err);
+            console.error('Error getting MySQL connection:', err);
             res.status(500).json({ success: false, message: 'Internal Server Error' });
             return;
         }
-        console.log('Testimonial inserted into CockroachDB:', result);
-        res.json({ success: true, message: 'Testimonial submitted successfully' });
+
+        // Insert the testimonial into the database
+        connection.query(sql, values, (queryErr, result) => {
+            // Release the connection back to the pool
+            connection.release();
+
+            if (queryErr) {
+                console.error('Error inserting into MySQL:', queryErr);
+                res.status(500).json({ success: false, message: 'Internal Server Error' });
+                return;
+            }
+
+            console.log('Testimonial inserted into MySQL:', result);
+            res.json({ success: true, message: 'Testimonial submitted successfully' });
+        });
     });
 });
 
 // Endpoint to retrieve testimonials from the database
 app.get('/testimonials', (req, res) => {
+    // Use the connection pool to query the database
     pool.query('SELECT * FROM testimonials', (error, results) => {
         if (error) {
             console.error('Error fetching testimonials:', error);
